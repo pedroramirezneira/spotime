a3715bf5f37a31b8d6bc080c6669b749bbb05220
Pedro Ramirez Neira <pedroramirezneira@gmail.com>
2025-06-13 21:33:30 -0300

    feat: added view model

--- app/src/main/java/com/mediaverse/spotime/notification/ScheduleNotificationViewModel.kt
+++ app/src/main/java/com/mediaverse/spotime/notification/ScheduleNotificationViewModel.kt
@@ -11,32 +11,34 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 @HiltViewModel
-class ScheduleNotificationViewModel @Inject constructor(
-    @ApplicationContext private val context: Context,
-): ViewModel() {
+class ScheduleNotificationViewModel
+    @Inject
+    constructor(
+        @ApplicationContext private val context: Context,
+    ) : ViewModel() {
+        @SuppressLint("ScheduleExactAlarm")
+        fun scheduleNotification() {
+            // Create an intent for the Notification BroadcastReceiver
+            val intent = Intent(context, NotificationReceiver::class.java)
 
-    @SuppressLint("ScheduleExactAlarm")
-    fun scheduleNotification() {
-        // Create an intent for the Notification BroadcastReceiver
-        val intent = Intent(context, NotificationReceiver::class.java)
+            // Create a PendingIntent for the broadcast
+            val pendingIntent =
+                PendingIntent.getBroadcast(
+                    context,
+                    0,
+                    intent,
+                    PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT,
+                )
 
-        // Create a PendingIntent for the broadcast
-        val pendingIntent = PendingIntent.getBroadcast(
-            context,
-            0,
-            intent,
-            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
-        )
+            // Get the AlarmManager service
+            val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
-        // Get the AlarmManager service
-        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-        // Get the selected time and schedule the notification
-        val time = 30000L
-        alarmManager.setExactAndAllowWhileIdle(
-            AlarmManager.RTC_WAKEUP,
-            time,
-            pendingIntent
-        )
+            // Get the selected time and schedule the notification
+            val time = 30000L
+            alarmManager.setExactAndAllowWhileIdle(
+                AlarmManager.RTC_WAKEUP,
+                time,
+                pendingIntent,
+            )
+        }
     }
-}
